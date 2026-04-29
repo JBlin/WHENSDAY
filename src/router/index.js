822d2c3eb35753879c83daec40f754749034c4f4
JAY <jnuin@bizbee.co.kr>
2026-04-29 15:42:41 +0900

    내 일정 잡기 버튼 오류

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MeetingView from '../views/MeetingView.vue'
+import ResultView from '../views/ResultView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: HomeView },
-    { path: '/meeting/:id', component: () => import('../views/MeetingView.vue') },
-    { path: '/meeting/:id/result', component: () => import('../views/ResultView.vue') },
+    { path: '/meeting/:id', component: MeetingView },
+    { path: '/meeting/:id/result', component: ResultView },
   ],
 })
 
